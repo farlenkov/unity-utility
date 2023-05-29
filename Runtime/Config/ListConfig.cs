2ea4ec4fad5f82bfed5db60c1f28dae923cd4a8b
Mike <work@farlenkov.com>
2023-05-29 10:14:05 +0300

    add SingleConfig & ListConfg

---
+++ Runtime/Config/ListConfig.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityConfig
+{
+    public abstract class ListConfig<T> 
+        : ScriptableObject where T : ListConfig<T>
+    {
+        static List<T> all;
+
+        public static List<T> All
+        {
+            get
+            {
+                if (all == null)
+                    all = new List<T>(Resources.LoadAll<T>(string.Empty));
+
+                return all;
+            }
+        }
+    }
+}
---
+++ Runtime/Config/SingleConfig.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace UnityConfig
+{
+    public abstract class SingleConfig<T> 
+        : ScriptableObject where T : SingleConfig<T>
+    {
+        static T instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var all = Resources.LoadAll<T>(string.Empty);
+
+                    if (all.Length > 0)
+                        instance = all[0];
+                }
+
+                return instance;
+            }
+        }
+    }
+}
