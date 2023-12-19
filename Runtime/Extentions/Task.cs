6f701315a26fba165ed3beeec5003fa5e51ac82e
Mike <work@farlenkov.com>
2023-12-19 15:46:54 +0400

    add Catch extension to UniTask

--- Runtime/Extentions/Task.cs
+++ Runtime/Extentions/Task.cs
@@ -80,6 +80,29 @@ namespace UnityUtility
 
 #if UNITY_2017_1_OR_NEWER
 
+        public static async UniTask<Exception> Catch(
+            this UniTask task,
+            bool writeLog = false)
+        {
+            try
+            {
+                await task;
+            }
+            catch (OperationCanceledException)
+            {
+                // nothing
+            }
+            catch (Exception ex)
+            {
+                if (writeLog)
+                    Log.Exception(ex);
+
+                return ex;
+            }
+
+            return default;
+        }
+
         public static async UniTask<(T, Exception)> Catch<T>(
             this UniTask<T> task,
             bool writeLog = false)
