ac9cd418282548ab5054ce42d9b1c9ee3ba2f8f3
mike <work@farlenkov.com>
2024-03-09 11:53:18 +0400

    add AsCoroutine to Task and UniTask

--- Runtime/Extentions/Task.cs
+++ Runtime/Extentions/Task.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 
@@ -120,5 +121,37 @@ namespace UnityUtility
 
             return (default, default);
         }
+
+#if UNITY_2017_1_OR_NEWER
+
+        // Not sure is this better than WaitUntil()
+        // but looks nicer on caller side.
+
+        public static IEnumerator AsCoroutine(this Task task)
+        {
+            if (task != null)
+                while (!task.IsCompleted)
+                    yield return null;
+        }
+
+        public static IEnumerator AsCoroutine<T>(this Task<T> task)
+        {
+            if (task != null)
+                while (!task.IsCompleted)
+                    yield return null;
+        }
+
+        public static IEnumerator AsCoroutine<T>(this UniTask<T>.Awaiter awaiter)
+        {
+            while (!awaiter.IsCompleted)
+                yield return null;
+        }
+
+        public static IEnumerator AsCoroutine(this UniTask.Awaiter awaiter)
+        {
+            while (!awaiter.IsCompleted)
+                yield return null;
+        }
+#endif
     }
 }
