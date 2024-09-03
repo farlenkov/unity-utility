dc365275a5629de3864ea5ed67f14c30be257d34
mike <work@farlenkov.com>
2024-09-03 14:23:15 +0400

    add more extentions

---
+++ Runtime/Extentions/HttpResponseMessage.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UnityUtility
+{
+    public static class HttpResponseMessageExt
+    {
+        public static async Task<T> ReadAsJson<T>(this HttpResponseMessage resp)
+        {
+            var content = await resp.Content.ReadAsStringAsync();
+            return JSON.parse<T>(content);
+        }
+    }
+}
---
+++ Runtime/Extentions/JToken.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json.Linq;
+
+namespace UnityUtility
+{
+    public static class JTokenExt
+    {
+        public static T Parse<T>(this JToken token, string key)
+        {
+            if (token == null)
+                return default;
+
+            var value = token[key];
+
+            if (value == null)
+                return default;
+            else
+                return JSON.parse<T>(value.ToString());                
+        }
+    }
+}
